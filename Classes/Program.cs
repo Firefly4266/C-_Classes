5ddb5435fd60d1ae3a1e8c891a4f51489c524d67
Firefly4266 <gaines.johnp@gmail.com>
2017-09-17 19:23:49 -0700

    Completed die roll exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,6 +10,19 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            Console.Write("\n Greetings user, please enter the number of times to roll the die: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int count = 0, total = 0;
+           
+            while (count < userInput)
+            {
+                int userRoll = random.Next(6) + 1;
+                total += userRoll;
+                count++;
+                Console.WriteLine(userRoll);
+            }
+            Console.WriteLine(total);
         }
     }
 }
